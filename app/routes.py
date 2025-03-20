6deaee05de5b5f1933dcca5c657dabd5570f3011
EdgarHuynhGit <huynhedgar@gmail.com>
2025-03-20 13:03:42 -0500

    add index

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Edgar'}
+    posts = [
+        {
+            'author': {'username': 'Michael'},
+            'body': 'My dogs are chirpin!'
+        },
+        {
+            'author': {'username': 'Carla'},
+            'body': 'F*** my chungus life man..'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
